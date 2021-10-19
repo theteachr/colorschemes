bcd2231793fb1294bbd816af92b066c75b0bc6be
Nick <sop.theteachr@gmail.com>
2021-10-20 00:25:43 +0530

    Create `dict` from kitty config

--- generate.py
+++ generate.py
@@ -31,8 +31,33 @@ def create_coolors_url(colors):
 	return 'https://coolors.co/{}'.format('-'.join(colors))
 
 
+def create_dict_from_kitty_conf(conf_file):
+	with open(conf_file) as f:
+		init = dict(map(lambda line: line.rstrip().split(),
+				f.readlines()))
+	
+	res = dict(background=init['background'][1:])
+	res.update({COLORS[i]: init[f'color{i}'][1:] for i in range(8)})
+
+	return res
+
+
+COLORS = [
+	'black',
+	'red',
+	'green',
+	'yellow',
+	'blue',
+	'magenta',
+	'cyan',
+	'white',
+]
+
 def main():
-	export_colors(gruvbox_material.COLORS, 'colors/css/colors.css')
+	#export_colors(gruvbox_material.COLORS, 'colors/css/colors.css')
+	colors = create_dict_from_kitty_conf('themes/ayu/kitty/ayu_mirage.conf')
+
+	print(create_coolors_url(colors.values()))
 
 
 if __name__ == '__main__':
