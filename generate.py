27be91fb709d69d9de339402b266150996cc7fb1
Nick <sop.theteachr@gmail.com>
2021-10-19 11:12:36 +0530

    Modularize
    Change to work with the new structure.

--- generate.py
+++ generate.py
@@ -1,13 +1,33 @@
 from itertools import starmap
-import gruvbox_material
 
-css_color_data = {
-    color: 'hsl({}, {}%, {}%)'.format(*values)
-    for color, values in gruvbox_material.COLORS.items()
-}
+from themes import (
+	ayu,
+	everforest,
+	gruvbox_material,
+	sonokai,
+	tokyonight,
+)
 
-root = ':root {{\n{}\n}}\n'.format(';\n'.join(starmap('\t--{}: {}'.format, css_color_data.items())))
+def make_root(color_dict):
+    css_color_data = {
+        color: 'hsl({}, {}%, {}%)'.format(*values)
+        for color, values in color_dict.items()
+    }
 
-with open('colors.css', 'w') as f:
-    f.write(root)
-    f.write('\n')
+    root = ':root {{\n{}\n}}\n'.format(
+            ';\n'.join(starmap('\t--{}: {}'.format, css_color_data.items())))
+
+    return root
+
+def export_colors(color_dict, out_file):
+    root = make_root(color_dict)
+
+    with open(out_file, 'w') as f:
+        f.write(root)
+        f.write('\n')
+
+def main():
+    export_colors(gruvbox_material.COLORS, 'colors/css/colors.css')
+
+if __name__ == '__main__':
+    main()
