16a78d400a88151b456af0bba623fb45e88d09b0
Nick <sop.theteachr@gmail.com>
2021-11-14 16:55:53 +0530

    Add helpers package

--- generate.py
+++ generate.py
@@ -24,10 +24,6 @@ def create_root(scheme_name, css_color_data):
 	).format(scheme_name)
 
 
-def create_coolors_url(colors):
-	return 'https://coolors.co/{}'.format('-'.join(colors))
-
-
 def create_dict_from_kitty_conf(conf_file):
 	with open(conf_file) as f:
 		init = dict(map(lambda line: line.rstrip().split(), f.readlines()))
@@ -72,6 +68,20 @@ def main():
 		f.writelines(roots)
 		f.write(color_classes)
 		f.write('\n')
+
+def test_coolors():
+    export = '''\
+--rich-black-fogra-29: hsla(205, 100%, 7%, 1);
+--prussian-blue: hsla(205, 100%, 16%, 1);
+--tart-orange: hsla(359, 100%, 65%, 1);
+--pistachio: hsla(89, 48%, 62%, 1);
+--medium-champagne: hsla(45, 60%, 74%, 1);
+--french-sky-blue: hsla(218, 90%, 74%, 1);
+--bright-lilac: hsla(280, 60%, 74%, 1);
+--middle-blue-green: hsla(170, 58%, 66%, 1);
+--alice-blue: hsla(204, 100%, 96%, 1);\
+'''
+    print(coolors.to_colors_dict(export))
 
 if __name__ == '__main__':
 	main()
