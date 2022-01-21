f404729a1dacecef1d1d39a75743b3b747221832
theteachr <sop.theteachr@gmail.com>
2022-01-21 19:07:15 +0530

    Use spaces

--- generate.py
+++ generate.py
@@ -2,12 +2,12 @@ from itertools import starmap
 from utils import coolors_export_to_color_dict, color_tuple_to_hsl
 
 from schemes import (
-	ayu,
-	everforest,
-	gruvbox_material,
-	nightfly,
-	sonokai,
-	tokyonight,
+    ayu,
+    everforest,
+    gruvbox_material,
+    nightfly,
+    sonokai,
+    tokyonight,
 )
 
 # TODO add scheme name in the `__init__` of the scheme package?
@@ -15,35 +15,34 @@ from schemes import (
 # TODO add cursor colors
 
 def create_root(scheme_name, css_color_data):
-	return ':root.{{}} {{{{\n{}\n}}}}\n'.format(
-		';\n'.join(starmap('\t--{}: {}'.format, css_color_data.items()))
-	).format(scheme_name)
+    return ':root.{{}} {{{{\n{}\n}}}}\n'.format(
+        ';\n'.join(starmap('\t--{}: {}'.format, css_color_data.items()))
+    ).format(scheme_name)
 
 
 
 SCHEMES = {
-	'Ayu Mirage': ayu,
-	'Everforest': everforest,
-	'Gruvbox Material': gruvbox_material,
-	'Sonokai Andromeda': sonokai,
-	'Tokyonight': tokyonight,
-	'Nightfly': nightfly,
+    'Ayu Mirage': ayu,
+    'Everforest': everforest,
+    'Gruvbox Material': gruvbox_material,
+    'Sonokai Andromeda': sonokai,
+    'Tokyonight': tokyonight,
+    'Nightfly': nightfly,
 }
 
 def main():
-	# define scheme colors
-	roots = [
-		create_root(
-			'-'.join(scheme_name.lower().split()),
-			{component: color_tuple_to_hsl(color) for component, color in mod.COLORS.items()}
-		)
-		for scheme_name, mod in SCHEMES.items()
-	]
+    # define scheme colors
+    roots = [
+        create_root(
+            '-'.join(scheme_name.lower().split()),
+            {component: color_tuple_to_hsl(color) for component, color in mod.COLORS.items()})
+        for scheme_name, mod in SCHEMES.items()
+    ]
 
-	# add color classes
-	color_classes = '\n'.join(map('.{0} {{\n\tbackground: var(--{0});\n}}'.format, COLORS))
+    # add color classes
+    color_classes = '\n'.join(map('.{0} {{\n\tbackground: var(--{0});\n}}'.format, COLORS))
 
-	with open('docs/css/colors.css', 'w') as f:
-		f.writelines(roots)
-		f.write(color_classes)
-		f.write('\n')
+    with open('docs/css/colors.css', 'w') as f:
+        f.writelines(roots)
+        f.write(color_classes)
+        f.write('\n')
--- utils.py
+++ utils.py
@@ -5,53 +5,54 @@ from itertools import starmap
 from constants import COLORS
 
 def create_coolors_url(colors):
-	return 'https://coolors.co/{}'.format('-'.join(colors))
+    return 'https://coolors.co/{}'.format('-'.join(colors))
 
 
 def coolors_export_to_color_dict(coolors_hsl_export):
-	pattern = re.compile('hsla\((\d+), (\d+)%, (\d+)%, 1\)')
+    pattern = re.compile('hsla\((\d+), (\d+)%, (\d+)%, 1\)')
 
-	return {
-		color: tuple(map(int, pattern.search(line).groups()))
-		for color, line in zip(['background'] + COLORS, coolors_hsl_export.split('\n'))
-	}
+    return {
+        color: tuple(map(int, pattern.search(line).groups()))
+        for color, line in zip(['background'] + COLORS, coolors_hsl_export.split('\n'))
+    }
 
 
 def color_tuple_to_hsl(three_tuple):
-	"""
-	Converts a (h, s, l) to a valid CSS value.
+    """
+    Converts a (h, s, l) to a valid CSS value.
 
-	h: hue
-	s: saturation (in %)
-	l: lightness (in %)
-	"""
+    h: hue
+    s: saturation (in %)
+    l: lightness (in %)
+    """
 
-	return 'hsl({}, {}%, {}%)'.format(*three_tuple)
+    return 'hsl({}, {}%, {}%)'.format(*three_tuple)
 
 
 def css_hsl_tuple_to_hex(color):
-	h, s, l = color
+    # FIXME Colors are washed out and dull
+    h, s, l = color
 
-	return ''.join(f'{round(v * 255):02X}' for v in colorsys.hsv_to_rgb(h/360, s/100, l/100))
+    return ''.join(f'{round(v * 255):02X}' for v in colorsys.hsv_to_rgb(h/360, s/100, l/100))
 
 
 def create_dict_from_kitty_conf(conf_file):
-	with open(conf_file) as f:
-		return dict(map(str.split, f.read().rstrip().split('\n')))
+    with open(conf_file) as f:
+        return dict(map(str.split, f.read().rstrip().split('\n')))
 
 
 def create_kitty_conf_from_coolors_hex_csv_export(line):
-	background, *rest = line.split(',')
+    background, *rest = line.split(',')
 
-	color_dict = {f'color{i}': color for i, color in enumerate(rest)}
-	color_dict.update(dict(background=background))
+    color_dict = {f'color{i}': color for i, color in enumerate(rest)}
+    color_dict.update(dict(background=background))
 
-	return '\n'.join(starmap('{} #{}'.format, color_dict.items()))
+    return '\n'.join(starmap('{} #{}'.format, color_dict.items()))
 
 
 def main():
-	coolors_export = '''\
---rich-black-fogra-29: hsla(205, 100%, 7%, 1);
+    coolors_export = '''\
+            --rich-black-fogra-29: hsla(205, 100%, 7%, 1);
 --prussian-blue: hsla(205, 100%, 16%, 1);
 --tart-orange: hsla(359, 100%, 65%, 1);
 --pistachio: hsla(89, 48%, 62%, 1);
@@ -60,18 +61,18 @@ def main():
 --bright-lilac: hsla(280, 60%, 74%, 1);
 --middle-blue-green: hsla(170, 58%, 66%, 1);
 --alice-blue: hsla(204, 100%, 96%, 1);\
-'''
-	color_dict = coolors_export_to_color_dict(coolors_export)
-	print(color_dict)
+        '''
+    color_dict = coolors_export_to_color_dict(coolors_export)
+    print(color_dict)
 
-	hexed = list(map(css_hsl_tuple_to_hex, color_dict.values()))
-	print(hexed)
+    hexed = list(map(css_hsl_tuple_to_hex, color_dict.values()))
+    print(hexed)
 
-	print(create_coolors_url(hexed))
+    print(create_coolors_url(hexed))
 
-	coolors_hex_export_line = '001626,003052,ff4e52,9ecc6e,e4d093,80acf8,c993e4,78dbcb,ebf7ff'
-	print(create_kitty_conf_from_coolors_hex_csv_export(coolors_hex_export_line))
+    coolors_hex_export_line = '001626,003052,ff4e52,9ecc6e,e4d093,80acf8,c993e4,78dbcb,ebf7ff'
+    print(create_kitty_conf_from_coolors_hex_csv_export(coolors_hex_export_line))
 
 
 if __name__ == '__main__':
-	main()
+    main()
