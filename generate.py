54901918ef0fbfb7adf14a9e53b81daff77de637
theteachr <sop.theteachr@gmail.com>
2023-07-22 19:17:36 +0530

    Use JSON and support variants

--- generate.py
+++ generate.py
@@ -1,30 +1,37 @@
+import json
+import os
+import random
+
 from dataclasses import dataclass
 from itertools import starmap
 from typing import Dict, List, Self, Tuple
 from constants import COLORS
 
-from schemes import (
-    ayu,
-    everforest,
-    gruvbox_material,
-    nightfly,
-    catppuccin,
-    sonokai,
-    tokyonight,
-    rose,
-)
-
-COLORSCHEME_MODS = [
-    ayu,
-    everforest,
-    gruvbox_material,
-    nightfly,
-    catppuccin,
-    sonokai,
-    tokyonight,
-    rose,
+COLORSCHEME_JSON_FILES = [
+    "schemes/{}/colors.json".format(d) for d in next(os.walk("schemes"))[1]
 ]
+
 CSS_PROPERTY_DELIMITER = ";\n"
+
+ENTRYPOINT_TEMPLATE = "templates/index.html"
+SITE_ENTRYPOINT = "docs/index.html"
+
+# def to_json(module):
+#     try:
+#         scheme_name, variant = module.NAME.split()
+#     except:
+#         scheme_name = module.NAME
+#         variant = "default"
+
+#     config = dict(name=module.NAME, colors={variant.lower(): module.COLORS})
+#     colors_json_file = os.path.join(module.__path__[0], "colors.json")
+
+#     with open(colors_json_file, "w") as f:
+#         json.dump(config, f, indent=2, sort_keys=True)
+
+
+# for m in COLORSCHEME_JSON_FILES:
+#     to_json(m)
 
 
 @dataclass(frozen=True)
@@ -44,27 +51,54 @@ class HSLColor:
 @dataclass(frozen=True)
 class Colorscheme:
     name: str
-    colors: Dict[str, HSLColor]
+    variants: Dict[str, Dict[str, HSLColor]]
+    # TODO: Enforce at least one variant, by adding an explicit instance variable.
+
+    @property
+    def default_variant(self) -> Tuple[str, Dict[str, HSLColor]]:
+        name, colors = self.variants.popitem()
+        self.variants[name] = colors
+        return name, colors
+
+    @property
+    def variant_names(self) -> List[str]:
+        return list(self.variants.keys())
 
     def to_css_root(self) -> str:
-        color_properties = starmap("\t--{}: {!r}".format, self.colors.items())
-        return f""":root.{hyphenate(self.name.lower())} {{
+        root_nodes = []
+
+        for variant, colors in self.variants.items():
+            color_properties = starmap("\t--{}: {!r}".format, colors.items())
+            root_node = f""":root.{self.hyphenated_name}-{variant} {{
 {CSS_PROPERTY_DELIMITER.join(color_properties)}
 }}
 """
+            root_nodes.append(root_node)
+
+        return "\n".join(root_nodes)
+
+    @property
+    def hyphenated_name(self) -> str:
+        return self.name.lower().replace(" ", "-")
 
     @classmethod
-    def from_module(cls, m) -> Self:
-        colors = {name: HSLColor.from_tuple(hsl) for name, hsl in m.COLORS.items()}
-        return cls(m.NAME, colors)
+    def from_json(cls, filepath):
+        with open(filepath) as f:
+            data = json.load(f)
+        scheme_name = data["name"]
+        variants_data = data["colors"]
+
+        variants = {}
+
+        for variant, colors in variants_data.items():
+            variants[variant] = {
+                color_name: HSLColor.from_tuple(t) for color_name, t in colors.items()
+            }
+
+        return cls(scheme_name, variants)
 
 
 # TODO add cursor colors
-# TODO add switchable schemes with variants (light/dark/solarized)
-
-
-def hyphenate(text: str) -> str:
-    return text.lower().replace(" ", "-")
 
 
 def generate_css(colorschemes: List[Colorscheme], css_out_file: str):
@@ -79,24 +113,40 @@ def generate_css(colorschemes: List[Colorscheme], css_out_file: str):
         f.write("\n")
 
 
-def generate_js(schemes: List, js_out_file: str):
-    with open("template.tjs") as f:
-        js_template = f.read()
+def generate_js(schemes: List[Colorscheme], out_file: str):
+    with open("templates/main.js") as f:
+        content = f.read()
 
-    class_name_pairs = [[hyphenate(scheme), scheme] for scheme in schemes]
-    num_schemes = len(class_name_pairs)
+    schemes_data = [
+        (scheme.name, scheme.hyphenated_name, scheme.variant_names)
+        for scheme in schemes
+    ]
 
-    js = js_template.format(num_schemes=num_schemes, class_name_pairs=class_name_pairs)
+    content = content.replace("'<schemes>'", json.dumps(schemes_data, indent=2))
+    print(content)
 
-    with open(js_out_file, "w") as f:
-        f.write(js)
+    with open(out_file, "w") as f:
+        f.write(content)
+
+
+def update_default_scheme(scheme: Colorscheme):
+    variant, _ = scheme.default_variant
+    scheme_class = f"{scheme.name.lower()}-{variant}"
+
+    with open(ENTRYPOINT_TEMPLATE) as f:
+        html = f.read()
+
+    with open(SITE_ENTRYPOINT, "w") as f:
+        f.write(html.format(scheme_name=scheme.name, scheme_class=scheme_class))
 
 
 def generate_docs():
-    generate_css(
-        [Colorscheme.from_module(m) for m in COLORSCHEME_MODS], "docs/css/colors.css"
-    )
-    generate_js([m.NAME for m in COLORSCHEME_MODS], "docs/main.js")
+    colorschemes = [Colorscheme.from_json(m) for m in COLORSCHEME_JSON_FILES]
+    entry_scheme = random.choice(colorschemes)
+
+    update_default_scheme(entry_scheme)
+    generate_css(colorschemes, "docs/css/colors.css")
+    generate_js(colorschemes, "docs/main.js")
 
 
 if __name__ == "__main__":
