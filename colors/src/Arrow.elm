7eac1036cb6b031a6cc0d2fae352a6eb2d2fa6ac
theteachr <sop.theteachr@gmail.com>
2023-08-12 02:35:09 +0530

    Wrap the path in a `g` element
    An attempt to make arrow rotations work on WebKit

--- colors/src/Arrow.elm
+++ colors/src/Arrow.elm
@@ -37,11 +37,12 @@ viewArrow arrow classes =
             [ width "24"
             , height "24"
             , fill "currentColor"
-            , transform rotate
             ]
-            [ Svg.path
-                [ d "M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z" ]
-                []
+            [ Svg.g [ transform rotate ]
+                [ Svg.path
+                    [ d "M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z" ]
+                    []
+                ]
             ]
         ]
 
